6d5b4c13d176fdc22b37c5c2b0c1b7638cfbd9bf
Jacqueline Silva <jacquegsilva@gmail.com>
2021-09-22 15:33:30 -0300

    segundo commmit

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -2,8 +2,15 @@
 
 context('Funcionalidade login', () =>{
 
+    beforeEach(() => {
+        cy.visit ('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+    
+        afterEach(() => {
+            cy.screenshot()
+        });
+
     it('Deve fazer login com sucess', () =>{
-      cy.visit ('http://lojaebac.ebaconline.art.br/minha-conta/')
       cy.get('#username') .type('aluno_ebac@teste.com')
       cy.get('#password') .type('teste@teste.com')
       cy.get('.woocommerce-form > .button') .click()
@@ -14,7 +21,6 @@ context('Funcionalidade login', () =>{
     })
    
     it('Deve exibir mensagem de erro ao inserir usuário inválido', () =>{
-      cy.visit ('http://lojaebac.ebaconline.art.br/minha-conta/')
       cy.get('#username') .type('aluno_ebac@teste')
       cy.get('#password') .type('teste@teste.com')
       cy.get('.woocommerce-form > .button') .click()
@@ -23,7 +29,6 @@ context('Funcionalidade login', () =>{
     })
 
     it('Deve exibir mensagem de erro ao inserir senha inválida', () =>{
-      cy.visit ('http://lojaebac.ebaconline.art.br/minha-conta/')
       cy.get('#username') .type('aluno_ebac@teste.com')
       cy.get('#password') .type('teste')
       cy.get('.woocommerce-form > .button') .click()
